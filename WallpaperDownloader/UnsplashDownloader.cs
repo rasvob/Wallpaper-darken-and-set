5f451d5f64a7420b959657eed1cb9bbb765825b0
Radek Svoboda <rasvob14@gmail.com>
2017-07-18 23:18:02 +0200

    Unsplash downloader fixed

--- WallpaperDownloader/UnsplashDownloader.cs
+++ WallpaperDownloader/UnsplashDownloader.cs
@@ -11,14 +11,23 @@ namespace WallpaperDownloader
         public override bool IsLinkValid()
         {
             var rx = new Regex(@"^https:\/\/unsplash\.com\/\?photo=(\w+)$");
-            return rx.IsMatch(Url);
+            var rxAlt = new Regex(@"^https:\/\/unsplash\.com\/photos\/(\w+)$");
+            return rx.IsMatch(Url) || rxAlt.IsMatch(Url);
         }
 
         public override string GetImageUrl()
         {
-            var rgx = new Regex(@"(https:\/\/unsplash\.com\/\?photo=)(?<id>\w+)");
+            var rgx = new Regex(@"(^https:\/\/unsplash\.com\/\?photo=)(?<id>\w+)$");
+            var rxAlt = new Regex(@"^https:\/\/unsplash\.com\/photos\/(?<id>\w+)$");
             Match match = rgx.Match(Url);
+
+            if (!match.Success)
+            {
+                match = rxAlt.Match(Url);
+            }
+
             string value = match.Groups["id"]?.Value ?? throw new ApplicationException(UnableToObtainLinkError);
+
             return $"http://unsplash.com/photos/{value}/download?force=true";
         }
     }
